2bd1cb04b832a346483ca46343e3c10bcea7c39d
Rajath Shetty <rajaths131@gmail.com>
2022-01-22 12:07:09 +0530

    Initial commit of template project.
    Created Template app for iOS.
    Added Unit test and Ui test target to project.

---
+++ PictureOfTheDay/PictureOfTheDay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PictureOfTheDay
+//
+//  Created by Rajath Shetty on 22/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
